f64c984f0ce252b84b5ea4ffed4b72d69c45cb44
Dima Ovharenko <dima-o@mail.ru>
2022-12-13 14:50:22 +0300

    Add media

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
-var swiper = new Swiper(".main__swiper", {
+let swiper = new Swiper(".main__swiper", {
     loop: true,
     spaceBetween: 0,
+});
+
+let burgerMenu = document.querySelector('.header__nav--menu');
+let burgerMenuClose = document.querySelector('.header__nav--close');
+let burgerlist = document.querySelector('.header__list--mobileBox');
+burgerMenu.addEventListener('click', function() {
+    this.classList.add("hidden");
+    burgerlist.classList.add("active");
+    burgerMenuClose.classList.add("active");
+});
+
+burgerMenuClose.addEventListener('click', function() {
+    this.classList.remove("active");
+    burgerlist.classList.remove("active");
+    burgerMenu.classList.remove("hidden");
 });
